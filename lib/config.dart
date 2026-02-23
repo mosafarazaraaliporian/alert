27c8f1986e3f8a1785472ea7cc0e0b4c095fa01c
mosafarazaraaliporian <mosafarazaraaliporian@users.noreply.github.com>
2026-02-23 18:45:21 +0000

    Connect app to production server at alertbot.gotdns.ch

--- lib/config.dart
+++ lib/config.dart
@@ -1,9 +1,6 @@
 class AppConfig {
-  // Change this to your server IP address
-  // For local testing on Android emulator: use 10.0.2.2
-  // For local testing on physical device: use your computer's IP (e.g., 192.168.1.100)
-  // For production: use your server domain
-  static const String apiBaseUrl = 'http://10.0.2.2:8000';
+  // Production server
+  static const String apiBaseUrl = 'http://alertbot.gotdns.ch';
   
   // App version
   static const String appVersion = '1.0.0';
